52527617fa6446aced6f60d552da05893c365e09
hanuruh <38216040+hanuruh@users.noreply.github.com>
2019-03-27 12:42:34 +0000

    Update main.c

--- main.c
+++ main.c
@@ -5,26 +5,29 @@
 
 int main(int argc, char **argv)
 {
-  /*******TESTAR************
+  /*******TESTAR************/
   Elem var1 = mkVar("x");
   Elem var2 = mkInt(1);
   Elem var3 = empty();
-  Instr inst1 = mkInstr(ATRIB,var1,var2,var3);
-  escreverDEBBUG(inst1);
+  Instr inst1 = mkInstr(ATRIB,var1,var2,var3); //x = 1
+  escrever(inst1);
   Elem var4 = mkVar("y");
   Elem var5 = mkInt(2);
-  Instr inst2 = mkInstr(ADD,var4,var1,var5);
-  escreverDEBBUG(inst2);
+  Instr inst2 = mkInstr(ADD,var4,var1,var5); //y = x + 2
+  escrever(inst2);
   Elem var6 = mkVar("z");
-  Instr inst3 = mkInstr(SUB,var6,var4,var1);
-  escreverDEBBUG(inst3);
+  Instr inst3 = mkInstr(SUB,var6,var4,var1); //z = y - x
+  escrever(inst3);
   Elem var7 = mkVar("w");
-  Instr inst4 = mkInstr(MUL,var7,var5,var4);
-  escreverDEBBUG(inst4);
-  **************************/
+  Instr inst4 = mkInstr(MUL,var7,var5,var4); //w = 2 * y
+  escrever(inst4);
+  /**************************/
+  
   
   /*********MAIN************/
+  /*
   
+  //ler linhas
   FILE * fp;
     char * line = NULL;
     size_t len = 0;
@@ -36,12 +39,17 @@ int main(int argc, char **argv)
     while ((read = getline(&line, &len, fp)) != -1) {
         printf("Retrieved line of length %zu:\n", read);
         printf("%s", line);
+        //Line --converter--> instrução
+        //instrução AddLast(LIST)
     }
 
     fclose(fp);
     if (line)
         free(line);
     exit(EXIT_SUCCESS);
+    
+    //executar (run())
+    * */
 	
 	return 0;
 }
