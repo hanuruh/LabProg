5508638e2a984692e2993616ca34095e0c5606be
hanuruh <38216040+hanuruh@users.noreply.github.com>
2019-04-08 23:08:37 +0100

    insert not working

--- trabalho.h
+++ trabalho.h
@@ -1,8 +1,12 @@
-#define HASH_SIZE 20
-#define MULTIPLIER 32
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define HASH_SIZE 100
+#define MULTIPLIER 31
 
 /*******************PROG_FUNC**********************/
-typedef enum {ATRIB, ADD, SUB, MUL, DIV, IF_I, PRINT, READ, GOTO_I, LABEL} OpKind;
+typedef enum {ATRIB, ADD, SUB, MUL, DIV, IF, PRINT, READ, GOTO, LABEL,START,QUIT} OpKind;
 
 typedef enum {EMPTY, INT_CONST, STRING} ElemKind;
 
@@ -18,45 +22,43 @@ typedef struct{
 typedef struct{
      OpKind op;
      Elem first, second, third;
+     int tipo, indice;
 } Instr;
 
 Elem mkVar(char *s);
 Elem mkInt(int n);
 Elem empty();
-Instr mkInstr(OpKind op, Elem x, Elem y, Elem z); //exemplo k = (mkInstr(ADD,mkVar("x"),mkVar("y"),mkInt(2)); y = x + 2 . k vai representar e memoria a instrução
-Instr convertToInstruc(char*);
+Instr mkInstr(OpKind op, Elem x, Elem y, Elem z, int a, int b); //exemplo k = (mkInstr(ADD,mkVar("x"),mkVar("y"),mkInt(2)); y = x + 2 . k vai representar e memoria a instrução
+Instr convertToInstruc(char*, int indice);
 int getValue(Elem x);
 int instrucType(char* str, char* ch); 
-char* getString(Elem x);
+char* getName(Elem x);
 void escrever(Instr intruc);
 void removeSpaces(char *str);
 /*******************PROG_FUNC**********************/
 
 /*******************LIST**********************/
-typedef struct list{ // fazer apontador a apontar para o ultimo elemento da lista ?
+typedef struct list{ 
 	Instr elem;
 	struct list *next;
-}PROG_LIST;
+}*PROG_LIST;
 
-PROG_LIST mkList(Instr head, PROG_LIST tail);
+PROG_LIST newList(Instr head, PROG_LIST tail);
 PROG_LIST tail(PROG_LIST l);
-PROG_LIST map(int (*f)(int), PROG_LIST l); 
-PROG_LIST filter(Bool (*p)(int), PROG_LIST l); //bool ou int ?
 PROG_LIST addLast(Instr n, PROG_LIST l1);
 PROG_LIST append(PROG_LIST l1, PROG_LIST l2);
-PROG_LIST readList();
-int head(PROG_LIST l);
+Instr head(PROG_LIST l);
 int length(PROG_LIST l);
-int elem_n(int n, PROG_LIST l);
+int getIndex(char* s, PROG_LIST lista);
 void printList(PROG_LIST l);
-void run(PROG_LIST l);
+int run(PROG_LIST l);
 /*******************LIST**********************/
 
 /*******************HASHTABLE**********************/
-typedef struct list{
+typedef struct hash{
 	char *chave;	
 	int elem;
-	struct list *next;
+	struct hash *next;
 }*PROG_CELL;
 
 unsigned int hash(char *s);
