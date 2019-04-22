3f5a02400f92dfd8f04d29c773a694f2b2b405d3
hanuruh <38216040+hanuruh@users.noreply.github.com>
2019-04-22 21:15:35 +0100

    Create hash.h

---
+++ hash.h
@@ -0,0 +1,34 @@
+#ifndef HASH_H
+#define HASH_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "list.h"
+#define HASH_SIZE 100
+#define MULTIPLIER 31
+
+/*
+union hash {
+  int value;
+  PROG_LIST label;
+};
+* */
+
+typedef struct hash{
+	char *chave;	
+	int elem;
+	struct hash *next;
+}*PROG_CELL;
+
+unsigned int hash(char *s);
+
+PROG_CELL table[HASH_SIZE];
+PROG_CELL lookup(char *s);
+void insert(char *s, int value);
+void init_table();
+void PrintCell(PROG_CELL c);
+int getValofCell(PROG_CELL p);
+char* getNameofCell(PROG_CELL p);
+
+#endif
