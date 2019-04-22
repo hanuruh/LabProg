6462acea77dff2b4f610ddd3c41c2360d479ba35
hanuruh <38216040+hanuruh@users.noreply.github.com>
2019-04-22 22:52:43 +0100

    Update list.h

--- list.h
+++ list.h
@@ -20,10 +20,10 @@ PROG_LIST tail(PROG_LIST l);
 PROG_LIST addLast(Instr n, PROG_LIST l1);
 PROG_LIST append(PROG_LIST l1, PROG_LIST l2);
 PROG_LIST last(PROG_LIST l);
+PROG_LIST file_to_list(char file_name[]);
 Instr head(PROG_LIST l);
 int length(PROG_LIST l);
 int getIndex(char* s, PROG_LIST lista);
 void printList(PROG_LIST l);
-int run(PROG_LIST l);
 
 #endif
