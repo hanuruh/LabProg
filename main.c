5509918997a2687a0e951e27ffb15d831071f66c
hanuruh <38216040+hanuruh@users.noreply.github.com>
2019-04-22 21:15:12 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "list.h"
+#include "hash.h"
+#include "instr.h"
+
+//usar apontadores na hash
+
+int main(int argc, char **argv){
+    init_table();
+    FILE * fp;
+    char line[1024];
+    char c;
+    size_t line_length = 1024;
+    ssize_t read;
+    int indexx = 1, i=0;
+    
+    fp = fopen(argv[1], "r");
+    if (fp == NULL){
+        printf("ERRO ABRIR FICHEIRO\n");        
+		return 0;
+	} 
+	
+    PROG_LIST lista = newList(mkInstr(START,empty(),empty(),empty(),0,0),NULL); //função à parte que mude isto
+    while ((fgets(line, sizeof(line), fp))!=NULL){
+			//printf("Linha = %s\n", line);
+			removeSpaces(line);
+			//printf("%s\n", line);
+			Instr instruc = convertToInstruc(line,indexx);
+			if(inst.op == LABEL){
+				lista = addLast(instruc, lista);
+				union hash new_label;
+				new_label.label = last(lista);	
+				
+			}
+			indexx++;
+			lista = addLast(instruc, lista);
+			
+	}
+	
+	
+	fclose(fp);
+	printf("Lista:\n");
+	printList(lista);
+	printf(".......................PRONTO A COMEÇAR........................\n");
+	run(lista);
+	
+	return 0;
+}
+
