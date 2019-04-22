019d304a74513517ef395907bc9df99cd3f0d46c
hanuruh <38216040+hanuruh@users.noreply.github.com>
2019-04-22 21:17:26 +0100

    Create hash.c

---
+++ hash.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "hash.h"
+
+unsigned int hash(char *s){
+		
+unsigned int h;
+
+unsigned char *p;
+
+h=0;
+
+for(p=(unsigned char *)s;*p!='\0';p++)
+	h= MULTIPLIER *h + *p;
+return h%HASH_SIZE;
+}
+
+PROG_CELL lookup(char *s){
+	int index = hash(s);
+  while (table[index] != NULL) {
+    if (table[index]->chave) {
+      return table[index];
+    }
+    index ++;
+    index %= HASH_SIZE;
+  }
+  return NULL;
+}
+
+
+void insert(char *s, int value){
+	int index;
+	index = hash(s);
+	PROG_CELL p;
+	p=(PROG_CELL)malloc(sizeof(struct list));
+	p->chave= s;
+	p->elem = value;
+	p->next = table[index];	
+	table[index] = p;
+	
+}
+
+void init_table(){
+	int index;
+	for(index=0;index<HASH_SIZE;index++)
+		table[index]=NULL;	
+}
+
+int getValofCell(PROG_CELL p){
+	return p->elem;
+}
+
+char* getNameofCell(PROG_CELL p){
+	return p->chave;
+}
